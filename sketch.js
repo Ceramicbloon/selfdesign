6f71747be3458ac04eb28e28929757ad30058b09
shivi09 <jainshivu09@gmail.com>
2022-06-02 05:48:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,55 +1,51 @@
+var player, crosshair, bullet, d, o, lins;
 
-
-var player, crosshair, bullet
-
-function preload() {
-
-}
+function preload() {}
 
 function setup() {
-	createCanvas(800, 800);
-	crosshair = createSprite(-100, -100, 20, 20)
+  createCanvas(800, 800);
+  crosshair = createSprite(-100, -100, 20, 20);
 
-
-	player = createSprite(200, 200, 20, 20)
-
-	//Create the Bodies Here.
-
-
-
+  player = createSprite(200, 200, 20, 20);
+  bullet = createSprite(player.x, player.y, 20, 20);
+  //Create the Bodies Here.
+  d = 0;
 }
-
 
 function draw() {
-	rectMode(CENTER);
-	background(0);
-	crosshair.x = mouseX
-	crosshair.y = mouseY
-	if (keyDown("W")) {
-		player.y = player.y - 5
-	} if (keyDown("A")) {
-		player.x = player.x - 5
-	} if (keyDown("S")) {
-		player.y = player.y + 5
-	} if (keyDown("D")) {
-		player.x = player.x + 5
-	}
+  rectMode(CENTER);
+  background(0);
+  crosshair.x = mouseX;
+  crosshair.y = mouseY;
+  if (keyDown("W")) {
+    player.y = player.y - 5;
+  }
+  if (keyDown("A")) {
+    player.x = player.x - 5;
+  }
+  if (keyDown("S")) {
+    player.y = player.y + 5;
+  }
+  if (keyDown("D")) {
+    player.x = player.x + 5;
+  }
 
-	stroke(255)
-	var lins = line(player.x, player.y, mouseX, mouseY)
+  stroke(255);
+  lins = line(player.x, player.y, mouseX, mouseY);
 
-	drawSprites();
-	if (mousePressedOver(crosshair)) {
-		bullet = createSprite(player.x, player.y, 20, 20)
-		bullet.setVelocity(1, 1)
-		bullet.x = crosshair.x
-		bullet.y = crosshair.y
+  drawSprites();
+  // if (mousePressedOver(crosshair)) {
 
-	}
-	console.log(crosshair.getDirection())
+  //   bullet.setSpeedAndDirection(
+  //     2,
+  //     atan2(crosshair.y - player.y, crosshair.x - player.x)
+  //   );
+  d += 2;
+  o = atan2(crosshair.y - player.y, crosshair.x - player.x);
+  bullet.x = d * cos(o) + player.x;
+  bullet.y = d * sin(o) + player.y;
 
+  // }
 
+  // console.log(lins.getDirection());
 }
-
-
-
